b503adfd81a80e7539c14935041e31cec16e5210
kartabayanov <kartabayanov@yandex.ru>
2016-12-01 15:30:27 +0300

    задание 4 'Форматирование json'

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,21 @@ import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding='UTF-8') as file:
+        return json.load(file)
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(json_data):
+    return json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        json_file = input('Введите путь до json файла: ')
+        print(pretty_print_json(load_data(json_file)))
+    except FileNotFoundError:
+        print('Файл не найден. Попробуйте ещё раз.')
+    except json.decoder.JSONDecodeError:
+        print('Файл не является JSON. Попробуйте ещё раз.')
+    except UnicodeDecodeError:
+        print('Ошибка кодировки. Данный должны быть в UTF-8')
